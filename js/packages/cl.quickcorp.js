a86b34fc295bcbde1367ecf37a8baa278b1b2ba9
Quickcorp <info@quickcorp.cl>
2018-11-01 16:06:21 -0300

    Using componentClass and controllerClass

--- js/packages/cl.quickcorp.js
+++ js/packages/cl.quickcorp.js
@@ -1,3 +1,10 @@
 Package('cl.quickcorp',[
-
+  Class('FooterController',Object,{
+    done:function (){
+      $(document).foundation();
+    }
+  }),
+  Class('FooterComponent',Component,{
+  })
 ]);
+var FOOTER;
